bf73f1bc343a91ab1430fbe40e5869d1a0befc8b
Pratik Vinayak More <86362195+Comder101@users.noreply.github.com>
2023-04-18 20:51:22 +0530

    "disallowed host issue fix"

--- Estorem/settings.py
+++ Estorem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&!4(+#rc)t8(f93p(k=nhak42-c@zn^5g^$(xcac)%ckh+#5&-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
