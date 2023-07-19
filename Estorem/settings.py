0958227f55182c80875f2de1408c00fc43496c99
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2023-07-19 13:06:44 +0530

    media root added in settings.py

--- Estorem/settings.py
+++ Estorem/settings.py
@@ -139,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'Admin-Dashboard/build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
